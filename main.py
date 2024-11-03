8bb43cb916b816a2babe4cc6fd232812ab1e4ef8
trance <trancedu1@gmail.com>
2024-11-03 13:57:41 -0500

    Define the video/config path in variable

--- main.py
+++ main.py
@@ -700,13 +700,19 @@ class ClickableSlider(QSlider):
 if __name__ == "__main__":
     debug_mode = True  # Set this to True to enable debug mode
     home_path = os.path.expanduser("~")
-    debug_video_path = os.path.join(home_path, r"Videos/S04.1080p.中英字幕/Fresh.Off.the.Boat.S04E01.1080p.AMZN.WEB.mp4")
 
-    # Define the config path
+    # Define the video and config path
+    hardcoded_video_path = r"Videos/S04.1080p.中英字幕/Fresh.Off.the.Boat.S04E01.1080p.AMZN.WEB.mp4"
+    mac_config_path = "Library/CloudStorage/OneDrive-Personal/Projects/config"
+    win_config_path = r"OneDrive/Projects/config"
+
+    debug_video_path = os.path.join(home_path, hardcoded_video_path)
     if platform.system() == "Darwin":  # Darwin is the system name for macOS
-        config_dir = os.path.join(home_path, "Library/CloudStorage/OneDrive-Personal/Projects/config")
+        config_dir = os.path.join(home_path, mac_config_path)
+    elif platform.system() == "Windows":
+        config_dir = os.path.join(home_path, win_config_path)
     else:
-        config_dir = os.path.join(home_path, r"OneDrive/Projects/config")
+        raise ValueError(f"Unsupported system: {platform.system()}")
 
     app = QApplication(sys.argv)
     window = VideoPlayerApp(debug=debug_mode, debug_video_path=debug_video_path, config_dir=config_dir)
