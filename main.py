f6d8893b6f05d3ceb9a28db096425cef9737496f
trance <trancedu1@gmail.com>
2024-12-14 11:41:04 -0500

    Add button play pause

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ from clip_manager import ClipManager
 from video_player import VideoPlayer
 
 class MainWindow(QWidget):
-    def __init__(self, video_player):
+    def __init__(self, video_player: VideoPlayer):
         super().__init__()
         self.setWindowTitle("English Listening Practice")
         self.video_player = video_player
@@ -31,6 +31,15 @@ class MainWindow(QWidget):
 
         self.setLayout(layout)
 
+        self.play_button = QPushButton("Play", self)
+        self.play_button.clicked.connect(self.video_player.play)
+        layout.addWidget(self.play_button)
+
+        self.pause_button = QPushButton("Pause", self)
+        self.pause_button.clicked.connect(self.video_player.pause)
+        layout.addWidget(self.pause_button)
+
+        self.resize(800, 600)
 
 def main():
     video_path = r"Videos/S04.1080p.中英字幕/Fresh.Off.the.Boat.S04E01.1080p.AMZN.WEB.mp4"
@@ -44,7 +53,6 @@ def main():
 
     app = QApplication(sys.argv)
     window = MainWindow(video_player)
-    window.create_video_widget()
     window.show()
     sys.exit(app.exec_())
 
